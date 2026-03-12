f684a85ec1eb958474961dc33de5d5e2b82abb62
阮久文 <rjw970725@163.com>
2026-03-12 20:54:43 +0800

    添加代码

--- src/main/java/com/wen/controller/FundValuationController.java
+++ src/main/java/com/wen/controller/FundValuationController.java
@@ -1,22 +1,95 @@
 package com.wen.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import com.wen.model.entity.FundValUserInfo;
+import com.wen.model.vo.*;
+import com.wen.service.FundValuationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Author : 青灯文案
- * @Date: 2026/3/9 23:13
+ * @author : rjw
+ * @date : 2026-03-12
  */
-@RestController("/fund/valuation")
-@Slf4j
+@RestController
+@RequestMapping("/fund/valuation")
+@RequiredArgsConstructor
 public class FundValuationController {
 
+    private FundValuationService fundValuationService;
 
-    @PostMapping("queryUserFunds")
-    public String queryUserFunds(){
-        log.info("查询用户基金");
-        return "查询用户基金成功";
+    /**
+     * 根据用户ID获取用户数据
+     */
+    @PostMapping("/queryUserInfo")
+    public Response<?> queryUserInfo(@RequestBody FundValUserIdRequest request) {
+        FundValUserInfo fundValUserInfo = fundValuationService.queryUserInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 根据用户ID修改用户数据
+     */
+    @PostMapping("/updateUserInfo")
+    public Response<?> updateUserInfo(@RequestBody FundValUserInfoRequest request) {
+        fundValuationService.updateUserInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 根据用户ID删除用户数据
+     */
+    @PostMapping("/deleteUserInfo")
+    public Response<?> deleteUserInfo(@RequestBody FundValUserIdRequest request) {
+        fundValuationService.deleteUserInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 新增持有基金数据
+     */
+    @PostMapping("/insertHoldingFundInfo")
+    public Response<?> insertHoldingFundInfo(@RequestBody FundValUserFundRequest request) {
+        fundValuationService.insertHoldingFundInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 新增自选基金数据
+     */
+    @PostMapping("/insertSelectionFundInfo")
+    public Response<?> insertSelectionFundInfo(@RequestBody FundValUserFundRequest request) {
+        fundValuationService.insertSelectionFundInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 删除基金数据
+     */
+    @PostMapping("/deleteFundInfo")
+    public Response<?> deleteFundInfo(@RequestBody FundValUserFundRequest request) {
+        fundValuationService.deleteFundInfo(request);
+        return Response.success();
+    }
+
+    /**
+     * 获取用户持有的基金
+     */
+    @PostMapping("/queryUserFundHolding")
+    public Response<?> queryUserFundHolding(@RequestBody FundValUserIdRequest request) {
+        fundValuationService.queryUserFundHolding(request);
+        return Response.success();
+    }
+
+    /**
+     * 获取用户自选的基金
+     */
+    @PostMapping("/queryUserFundSelection")
+    public Response<?> queryUserFundSelection(@RequestBody FundValUserIdRequest request) {
+        fundValuationService.queryUserFundSelection(request);
+        return Response.success();
     }
 
 }
--- src/main/java/com/wen/service/FundValuationService.java
+++ src/main/java/com/wen/service/FundValuationService.java
@@ -1,8 +1,53 @@
 package com.wen.service;
 
+import com.wen.model.entity.FundValUserInfo;
+import com.wen.model.vo.FundValUserFundRequest;
+import com.wen.model.vo.FundValUserIdRequest;
+import com.wen.model.vo.FundValUserInfoRequest;
+
 /**
- * @Author : 青灯文案
- * @Date: 2026/3/10 21:46
+ * @author : rjw
+ * @date : 2026-03-12
  */
 public interface FundValuationService {
+
+    /**
+     * 根据用户ID获取用户信息
+     */
+    FundValUserInfo queryUserInfo(FundValUserIdRequest request);
+
+    /**
+     * 根据用户ID修改用户信息
+     */
+    void updateUserInfo(FundValUserInfoRequest request);
+
+    /**
+     * 根据用户ID删除用户信息
+     */
+    void deleteUserInfo(FundValUserIdRequest request);
+
+    /**
+     * 新增持有基金数据
+     */
+    void insertHoldingFundInfo(FundValUserFundRequest request);
+
+    /**
+     * 新增自选基金数据
+     */
+    void insertSelectionFundInfo(FundValUserFundRequest request);
+
+    /**
+     * 删除基金数据
+     */
+    void deleteFundInfo(FundValUserFundRequest request);
+
+    /**
+     * 查询用户持有的基金
+     */
+    void queryUserFundHolding(FundValUserIdRequest request);
+
+    /**
+     * 查询用户自选的基金
+     */
+    void queryUserFundSelection(FundValUserIdRequest request);
 }
